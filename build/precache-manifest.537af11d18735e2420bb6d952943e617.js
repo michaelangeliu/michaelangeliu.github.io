c42c0aa797b65e7737fb96ba70b158f95e188afa
Michael Liu <michaelangeliu+git@gmail.com>
2020-02-02 14:27:21 -0600

    Update build process for github pages user page
    - https://create-react-app.dev/docs/deployment/#github-pages
    - Changed homepage url and deploy script
    /

---
+++ build/precache-manifest.537af11d18735e2420bb6d952943e617.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f535af0d1d6e6b7a36d38c872a3edcd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "81d9c5b5ab386647a578",
+    "url": "/static/css/main.0c86f8ec.chunk.css"
+  },
+  {
+    "revision": "aa5e23d213b7dc6c9173",
+    "url": "/static/js/2.9494ceef.chunk.js"
+  },
+  {
+    "revision": "99dfaa1db1f21a60ca117425c6c404b2",
+    "url": "/static/js/2.9494ceef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81d9c5b5ab386647a578",
+    "url": "/static/js/main.60a5d81f.chunk.js"
+  },
+  {
+    "revision": "575d3f19c0806ea1a0ec",
+    "url": "/static/js/runtime-main.f99909bf.js"
+  },
+  {
+    "revision": "b2dbe9f618c931cea9b842025450c919",
+    "url": "/static/media/facebook.b2dbe9f6.svg"
+  },
+  {
+    "revision": "6399fb8e6478601e63967457f86d2414",
+    "url": "/static/media/github.6399fb8e.svg"
+  },
+  {
+    "revision": "9b6ae4c9689db91e58a307bf2405d188",
+    "url": "/static/media/instagram.9b6ae4c9.svg"
+  },
+  {
+    "revision": "2c52441b11ee568042dff6e9a4d0e712",
+    "url": "/static/media/linkedin.2c52441b.svg"
+  },
+  {
+    "revision": "1a68083a06e5f047b1a20b49ca9ced25",
+    "url": "/static/media/tumblr.1a68083a.svg"
+  },
+  {
+    "revision": "4d648778dd361a0c21ca696498b6cfe8",
+    "url": "/static/media/twitter.4d648778.svg"
+  }
+]);
