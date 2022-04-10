b9e7e6a16f9ff07a4383340ab804dfad8dbbe085
Michael Liu <5413532+michaelangeliu@users.noreply.github.com>
2022-04-10 15:36:42 -0500

    ci: implement commitlint
    - Add commitlint to standardize commit messages

---
+++ commitlint.config.js
@@ -0,0 +1,81 @@
+module.exports = {
+	extends: ['@commitlint/config-conventional'],
+	rules: {
+		'body-leading-blank': [
+			1,
+			'always',
+		],
+		'body-max-line-length': [
+			2,
+			'always',
+			100,
+		],
+		'footer-leading-blank': [
+			1,
+			'always',
+		],
+		'footer-max-line-length': [
+			2,
+			'always',
+			100,
+		],
+		'header-max-length': [
+			2,
+			'always',
+			100,
+		],
+		'scope-case': [
+			2,
+			'always',
+			'lower-case',
+		],
+		'subject-case': [
+			2,
+			'never',
+			[
+				'sentence-case',
+				'start-case',
+				'pascal-case',
+				'upper-case',
+			],
+		],
+		'subject-empty': [
+			2,
+			'never',
+		],
+		'subject-full-stop': [
+			2,
+			'never',
+			'.',
+		],
+		'type-case': [
+			2,
+			'always',
+			'lower-case',
+		],
+		'type-empty': [
+			2,
+			'never',
+		],
+		'type-enum': [
+			2,
+			'always',
+			[
+				'build', // Changes that affect the build system or external dependencies (example scopes: gulp, broccoli, npm)
+				'chore',
+				'ci', // Changes to our CI configuration files and scripts (example scopes: Travis, Circle, BrowserStack, SauceLabs)
+				'docs', // Documentation only changes
+				'feat', // New feature
+				'fix', // Bug fix
+				'perf', // Changes that improve performance
+				'refactor', // Changes that neither fixes a bug, nor adds a feature
+				'revert',
+				'style', // Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc.)
+				'test', // Adding missing tests or correcting existing tests
+				'translation',
+				'security',
+				'changeset',
+			],
+		],
+	},
+};
